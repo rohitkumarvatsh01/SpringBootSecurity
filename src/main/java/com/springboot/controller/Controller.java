58fb9ec2942d2e98cdf46d2908f3a1075ba5cbe5
Rohit Kumar <rohitk@del.aithent.com>
2024-06-14 17:14:03 +0530

    Add Custom Authentication

--- src/main/java/com/springboot/controller/Controller.java
+++ src/main/java/com/springboot/controller/Controller.java
@@ -5,16 +5,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+//@RequestMapping("/api")
 public class Controller {
 	
+	@GetMapping("/")
+	public String home() {
+		return "Welcome to Home Page";
+	}
+	
+	@GetMapping("/admin")
+	public String admin() {
+		return "Welcome to Admin Page";
+	}
+	
 	@GetMapping("/user")
-    public String helloUser() {
-        return "Hello User";
-    }
+	public String user() {
+		return "Welcome to User Page";
+	}
 
-    @GetMapping("/admin")
-    public String helloAdmin() {
-        return "Hello Admin";
-    }
 }
