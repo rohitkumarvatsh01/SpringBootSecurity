35499ef3377e5ff9f254f9cca3ea471225ed4e04
Rohit Kumar <85385695+rohitkumarvatsh01@users.noreply.github.com>
2024-06-27 23:51:09 +0530

    Update Controller.java

--- src/main/java/com/springboot/controller/Controller.java
+++ src/main/java/com/springboot/controller/Controller.java
@@ -2,7 +2,7 @@ package com.springboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController; 
 
 @RestController
 //@RequestMapping("/api")
